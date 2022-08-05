d33fcb33c9d24cdf403a152037c9331d23fc3f23
Adina Feinstein <adina.d.feinstein@gmail.com>
2022-08-05 08:24:49 -0500

    more plotting utils

--- nirhiss/plotting.py
+++ nirhiss/plotting.py
@@ -3,67 +3,19 @@ import h5py
 import os, sys
 import numpy as np
 from astropy.io import fits
-from scipy.signal import savgol_filter
-from scipy.ndimage import gaussian_filter
-from tqdm import tqdm_notebook
-import matplotlib.pyplot as plt
-import astraeus.xarrayIO as xrio
-
-from scipy.io import wavfile
-from scipy.interpolate import interp2d, interp1d
-from scipy.interpolate import NearestNDInterpolator
-
-import ccdproc as ccdp
-from astropy import units
-from astropy.nddata import CCDData
-from tqdm import tqdm_notebook
-from scipy.interpolate import interp1d
 from astropy.table import Table
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 from .utils import bin_at_resolution
 
-from matplotlib.colors import ListedColormap
-pmap = ListedColormap(np.load('/Users/belugawhale/parula_data.npy'))
-colors = np.load('/Users/belugawhale/parula_colors.npy')
+__all__ = ['stacked_transits', 'transmission_spectrum', 'transit_residuals']
 
-COLOR = 'k'
-plt.rcParams['font.size'] = 20
-plt.rcParams['text.color'] = COLOR
-plt.rcParams['axes.labelcolor'] = COLOR
-plt.rcParams['xtick.color'] = COLOR
-plt.rcParams['ytick.color'] = COLOR
-
-plt.rcParams['xtick.major.width'] = 3
-plt.rcParams['ytick.major.width'] = 3
-plt.rcParams['xtick.major.size']  = 10 #12
-plt.rcParams['ytick.major.size']  = 10 #12
-
-plt.rcParams['xtick.minor.width'] = 1
-plt.rcParams['ytick.minor.width'] = 1
-plt.rcParams['xtick.minor.size']  = 6
-plt.rcParams['ytick.minor.size']  = 6
-
-plt.rcParams['axes.linewidth'] = 3
-
-plt.rcParams['font.size'] = 20
-plt.rcParams['text.color'] = COLOR
-plt.rcParams['xtick.color'] = COLOR
-plt.rcParams['ytick.color'] = COLOR
-plt.rcParams['axes.labelcolor'] = COLOR
-plt.rcParams['axes.spines.top'] = True
-plt.rcParams['axes.spines.right'] = True
-plt.rcParams['axes.labelcolor'] = COLOR
-plt.rcParams['axes.edgecolor'] = COLOR
-plt.rcParams['figure.facecolor'] = 'none'
-plt.rcParams['legend.facecolor'] = 'none'
-
-__all__ = ['stacked_transits', 'transmission_spectrum']
-
-def stacked_transits(time, wavelength, flux, variance,
+def stacked_transits(time, wavelength, flux, variance, colors,
                      centers=np.flip(np.linspace(0.85, 2.55, 15)),
                      offset=0.05, offset_delta=0.005, figsize=(8,14),
                      text=True, time_ind=230, linestyle=''):
-    global colors
+
     wave_offset = 0.5
 
     x = 0
@@ -160,7 +112,7 @@ def transmission_spectrum(wavelength, depth, wave_err, depth_err,
 
 def ers_transmission_spectra(table, order, color, label, ax, alpha=0.4, lw=3,
                              upper_lim2=0.85, plot_type='fill_between',
-                             binned=True, R=100):
+                             binned=True, R=100, ms=10):
     """
     Plotting all of the beautiful transmission spectra from the ERS program.
 
@@ -218,13 +170,84 @@ def ers_transmission_spectra(table, order, color, label, ax, alpha=0.4, lw=3,
                                        'label':label})
 
     elif plot_type == 'scatter':
-        plot_type_scatter(out[0], out[1], table['wave_error'], out[2],
+        plot_type_scatter(out[0], out[1], np.zeros(len(out[1])), out[2],
                           ax=ax,
                           kwargs={'alpha':1, 'label':label,
                                   'zorder':10, 'color':color,
-                                  'linestyle':''})
+                                  'linestyle':'', 'ms':ms})
     else:
         return('plot_type not implemented. Please select between "scatter"\
                 and "fill_between".')
+
+    return
+
+def transit_residuals(time, flux, flux_err, residuals, residuals_err, color,
+                      model=None, ax=None, size="20%", pad=0, index1=0, index2=0):
+    """
+    Helper function to create subplot with a transit and the residuals
+    underneath.
+
+    Parameters
+    ----------
+    time : np.ndarray
+       Array of times.
+    flux : np.ndarray
+       Array of fluxes.
+    flux_err : np.ndarray
+       Array of flux errors.
+    residuals : np.ndarray
+       Array of residuals between the light curve and best-fit model.
+    residuals_err : np.ndarray
+       Array of errors on the residuals.
+    color : str
+       What color to plot the data in.
+    model : np.ndarray, optional
+       Best-fit transit model to overplot. Default is None (i.e. the model
+       will not be overplotted).
+    ax : matplotlib.axes._subplots.AxesSubplot, optional
+       The subplot to plot the data on. Default is None (i.e. will create its
+       own figure).
+    size : str, optional
+       The percent split between the main light curve and the residuals. Default
+       is '20%'.
+    pad : float, optional
+       The amount of padding between the light curve and residuals subpanels.
+       Default is 0.
+    index1 : int, optional
+       If looping through several columns, this will help with setting/removing
+       axes labels and ticklabels. Default is 0. Would be "i" in the first loop.
+    index2 : int, optional
+       If looping through several rows, this will help with setting/removing
+       axes labels and ticklabels. Default is 0. Would be "j" in the second loop.
+    """
+    if ax is None:
+        fig, ax = plt.subplots(figsize=(10,6))
+
+    divider = make_axes_locatable(ax)
+    ax2 = divider.append_axes("bottom", size=size, pad=pad)
+    ax.figure.add_axes(ax2)
+
+    ax.errorbar(time, flux, yerr=flux_err, marker='.', linestyle='',
+                color=color_dict[key])
+    ax.plot(time, model, 'k', zorder=3)
+    ax2.errorbar(time, residuals, yerr=residuals_err,
+                 marker='.', linestyle='', color=color_dict[key])
+    ax.set_xticks([])
+
+    if index1==0:
+        ax2.set_xticklabels([])
+        if index2==0:
+            ax2.set_ylabel('Residuals', fontsize=16)
+    if index2 > 0:
+        ax2.set_yticklabels([])
+    if index1==1 and index2==0:
+        ax2.set_ylabel('Residuals', fontsize=16)
+    if index1==1 and index2==2:
+        ax2.set_xlabel('Time from Mid-Transit [hrs]')
+
+    ax2.set_ylim(resid_lims)
+    ax.set_ylim(lc_lims)
+    ax.set_xlim(xlim)
+    ax2.set_xlim(xlim)
 
     return
